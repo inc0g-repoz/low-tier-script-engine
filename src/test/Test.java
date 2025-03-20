dc72e2fef48f9c50c86897b6bae04bddae4c656e
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2025-03-21 05:25:00 +0600

    Move example scripts to a separate directory

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,40 +1,45 @@
 package test;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.Proxy;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
+import java.util.LinkedList;
 import java.util.function.Supplier;
 
 import com.github.inc0grepoz.ltse.Script;
 import com.github.inc0grepoz.ltse.ScriptExecutor;
-import com.github.inc0grepoz.ltse.util.FlowControl;
-import com.github.inc0grepoz.ltse.util.Reflection;
+import com.github.inc0grepoz.ltse.util.Lexer;
 
-@SuppressWarnings({"rawtypes", "unused"})
+@SuppressWarnings("unused")
 public class Test
 {
 
+    private static final ScriptExecutor EXECUTOR = new ScriptExecutor();
+    private static final File LOADER_DIRECTORY;
+
+    static
+    {
+        EXECUTOR.setLoaderDirectory(LOADER_DIRECTORY = new File("scripts"));
+    }
+
     public static void main(String[] args)
     {
+//      testLexer();
         testScriptEngine();
 //      test();
     }
 
-    private static void test()
+    private static void testLexer()
     {
+        // src/com/github/inc0grepoz/dsl/util/Lexer.java
+        // main.script
+        File file = new File(LOADER_DIRECTORY, "main.script");
+
         try
         {
-            List<String> list = Arrays.asList("1", "2", "3");
-            Method method = list.getClass().getMethod("forEach", Consumer.class);
-            method.setAccessible(true);
+            LinkedList<String> tokens = Lexer.readTokens(new FileReader(file));
+            tokens.forEach(token -> System.out.print("$" + token));
+            System.out.println();
         }
         catch (Throwable t)
         {
@@ -44,17 +49,12 @@ public class Test
 
     private static void testScriptEngine()
     {
-        ScriptExecutor executor = new ScriptExecutor();
-
-        // src/com/github/inc0grepoz/dsl/util/Lexer.java
-        // test
-        File file = new File("test.script");
-        Supplier[] getter = (Supplier[]) Array.newInstance(Supplier.class, 1);
+        File file = new File(LOADER_DIRECTORY, "main.script");
 
         Script script = time("Compiled", () -> {
             try
             {
-                return executor.load(file);
+                return EXECUTOR.load(file);
             }
             catch (IOException e)
             {
@@ -63,16 +63,6 @@ public class Test
         });
 
         Object rv = time("Executed", () -> script.callFunction("main"));
-
-        for (int i = 0; i < 10; i++)
-        {
-            time("Executed", () -> script.callFunction("main"));
-        }
-
-        if (rv != FlowControl.VOID)
-        {
-            System.out.println("Returned " + rv);
-        }
     }
 
     private static <T> T time(Supplier<T> lambda)
