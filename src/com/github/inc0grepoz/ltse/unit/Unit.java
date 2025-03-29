5547d1811edea5fde46db5f7cb60760cab2ab903
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2025-03-29 13:15:24 +0600

    Add javadocs to class Unit

--- src/com/github/inc0grepoz/ltse/unit/Unit.java
+++ src/com/github/inc0grepoz/ltse/unit/Unit.java
@@ -1,5 +1,11 @@
 package com.github.inc0grepoz.ltse.unit;
 
+/**
+ * Represents a callable unit of scripted logic that can be
+ * invoked multiple times
+ * 
+ * @author inc0g-repoz
+ */
 public abstract class Unit
 {
 
@@ -15,11 +21,25 @@ public abstract class Unit
         }
     }
 
+    /**
+     * Returns the root unit of the script where this unit
+     * has been compiled.
+     * 
+     * @return the root unit of the script
+     */
     UnitRoot root()
     {
         return parent == null ? (UnitRoot) this : parent.root();
     }
 
+    /**
+     * Executes this unit with all of it's child units (if
+     * it is a section) once and returns an object, if has
+     * to, or {@link FlowControl#VOID VOID} otherwise.
+     * 
+     * @param context the current context of execution
+     * @return an object instance
+     */
     Object execute(ExecutionContext context)
     {
         throw new UnsupportedOperationException("No " + getClass() + " implementation");
