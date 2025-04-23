96484b538c5ac483dbcb182a898e66255aaf4630
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2025-04-24 01:40:35 +0600

    Update boot.bat

--- boot/boot.bat
+++ boot/boot.bat
@@ -1,16 +1,19 @@
 @ECHO OFF
 SETLOCAL
 
-:: Check if lix4j.jar exists in script's directory
 IF NOT EXIST "%~dp0lix4j.jar" (
     ECHO Downloading lix4j.jar...
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/inc0g-repoz/lix4j/releases/latest/download/lix4j.jar', '%~dp0lix4j.jar')"
 
-    :: Validating
-    IF NOT EXIST "%~dp0lix4j.jar" (
+    IF EXIST "%~dp0lix4j.jar" (
+        ECHO Successfullly downloaded
+    ) ELSE (
         ECHO Failed to download lix4j.jar
         EXIT /b 1
     )
+
+    timeout /t 1 /nobreak>NUL
+    CLS
 )
 
 java -jar "%~dp0lix4j.jar" %*
