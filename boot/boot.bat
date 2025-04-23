4f56c8bc44106d32a0a21153498a0081b39ccc88
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2025-04-24 02:06:14 +0600

    Update boot.bat

--- boot/boot.bat
+++ boot/boot.bat
@@ -1,11 +1,18 @@
 @ECHO OFF
 SETLOCAL
 
-IF NOT EXIST "%~dp0lix4j.jar" (
-    ECHO Downloading lix4j.jar...
-    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/inc0g-repoz/lix4j/releases/latest/download/lix4j.jar', '%~dp0lix4j.jar')"
+SET DOWNLOAD_URL=https://github.com/inc0g-repoz/lix4j/releases/latest/download/lix4j.jar
+SET LIX4J_PATH=%~dp0lix4j.jar
 
-    IF EXIST "%~dp0lix4j.jar" (
+IF [%1]==[] (
+    GOTO usage
+)
+
+IF NOT EXIST "%LIX4J_PATH%" (
+    ECHO Downloading lix4j.jar...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%LIX4J_PATH%')"
+
+    IF EXIST "%LIX4J_PATH%" (
         ECHO Successfullly downloaded
     ) ELSE (
         ECHO Failed to download lix4j.jar
@@ -17,3 +24,9 @@ IF NOT EXIST "%~dp0lix4j.jar" (
 )
 
 java -jar "%~dp0lix4j.jar" %*
+GOTO eof
+
+:usage
+ECHO Usage: %~n0 script_file [args...]
+
+:eof
