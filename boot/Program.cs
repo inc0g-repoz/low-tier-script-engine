807e10d90146c02522db1625edaba122cbb13fa6
Daniil <46551793+inc0g-repoz@users.noreply.github.com>
2025-04-24 00:47:26 +0600

    Include a project for building a boot executable

---
+++ boot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lix4j_boot {
+
+    class Program {
+
+        static void Main(string[] args) {
+            var processInfo = new ProcessStartInfo() {
+                FileName = "java.exe",
+                Arguments = "-jar lix4j.jar " + string.Join(" ", args),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            Process proc;
+            if ((proc = Process.Start(processInfo)) == null) {
+                throw new InvalidOperationException("??");
+            }
+
+            while (!proc.StandardOutput.EndOfStream) {
+                string line = proc.StandardOutput.ReadLine();
+                Console.WriteLine(line);
+            }
+
+            proc.WaitForExit();
+            int exitCode = proc.ExitCode;
+            proc.Close();
+        }
+
+    }
+
+}
